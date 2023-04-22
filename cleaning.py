5bfc04a6b03727865fa9f6f6bc518fdd04d3c973
ZizzyDizzyMC <ZizzyDizzyMC@gmail.com>
2023-04-22 16:10:31 -0400

    Update to re.finditer
    This helps speed up processing of large data sets.
    I'm still working on getting even larger datasets working .

--- cleaning.py
+++ cleaning.py
@@ -8,32 +8,30 @@ args = parser.parse_args()
 f = open(args.file, "r")
 dump = f.read()
 f.close()
-horribleidea = re.findall(r"(?:(?:(?:https?:\\?\/\\?\/)?(?:www\.|[im]\.)?imgur(?:\.|(?:.?dot.?)|.)?(?:com|io)(?:\\\/|\\|\/|.?slash|(?:.?slash|.?\/)(?:\w|[^\S\r\n]|%2F)))?,(?:(?!gallery|search)(?:r\/\w+\/)?(\w{7}|\w{5})[bghlmrst]?)|(?:(?:https?:\\?\/\\?\/)?(?:www\.|[im]\.)?(?:i\.stack\.)imgur(?:\.|(?:.?dot.?)|.)?(?:com|io)\\?\/?)(?:(?!gallery|search)(?:r\/\w+\/)?(\w{7}|\w{5})[bghlmrst]?)|(?:(?:https?:\\?\/\\?\/)?(?:www\.|[im]\.)?(?<!(?:i\.stack\.))imgur(?:\.|(?:.?dot.?)|.)?(?:com|io)(?:\\\/|\\|\/|.?slash|(?:.?slash|.?\/)(?:\w|[^\S\r\n])|%2F))(?:(?!gallery|search)(?:r\/\w+\/)?(?:(?:original|mp4)(?:\\\/|\\|\/|.?slash|(?:.?slash|.?\/)(?:\w|[^\S\r\n])|%2F).+?|download(?:\\\/|\\|\/|.?slash|(?:.?slash|.?\/)(?:\w|[^\S\r\n])|%2F)?)?(\w{7}|\w{5})[bghlmrst]?|(?:a\/?\\?(\w{7}|\w{5}))|(?:(?:gallery|t\/\w+)\/(\w{7}|\w{5}))|(?:(?:user\/([^\/?#]+)(?:\", )+?(?:\/posts|\/submitted)?\/?)|(?:user\/([^\/?#]+)(?:\/posts|\/submitted)\/?)|(?:user\/([^\/?#]+)(?:\/favorites)\/?))))", dump)
 with open('links.txt', 'a') as f:
-  for i in range(len(horribleidea)):
-    current = horribleidea[i]
-    if current[0]:
-      print('https://imgur.com/' + current[0])
-      f.write('https://imgur.com/' + current[0] + '\n')
-    if current[1]:
-      print('https://i.stack.imgur.com/' + current[1] + '.png')
-      f.write('https://i.stack.imgur.com/' + current[1] + '.png' + '\n')
-    if current[2]:
-      print('https://imgur.com/' + current[2])
-      f.write('https://imgur.com/' + current[2] + '\n')
-    if current[3]:
-      print('https://imgur.com/a/' + current[3])
-      f.write('https://imgur.com/a/' + current[3] + '\n')
-    if current[4]:
-      print('https://imgur.com/gallery/' + current[4])
-      f.write('https://imgur.com/gallery/' + current[4] + '\n')
-    if current[5]:
-      print('https://imgur.com/user/' + current[5])
-      f.write('https://imgur.com/user/' + current[5] + '\n')
-    if current[6]:
-      print('https://imgur.com/user/' + current[6]+ '/posts')
-      f.write('https://imgur.com/user/' + current[6]+ '/posts' + '\n')
-    if current[7]:
-      print('https://imgur.com/user/' + current[7]+ '/favorites')
-      f.write('https://imgur.com/user/' + current[7]+ '/favorites' + '\n')
+  for i in re.finditer(r"(?:(?:(?:https?:\\?\/\\?\/)?(?:www\.|[im]\.)?imgur(?:\.|(?:.?dot.?)|.)?(?:com|io)(?:\\\/|\\|\/|.?slash|(?:.?slash|.?\/)(?:\w|[^\S\r\n]|%2F)))?,(?:(?!gallery|search)(?:r\/\w+\/)?(\w{7}|\w{5})[bghlmrst]?)|(?:(?:https?:\\?\/\\?\/)?(?:www\.|[im]\.)?(?:i\.stack\.)imgur(?:\.|(?:.?dot.?)|.)?(?:com|io)\\?\/?)(?:(?!gallery|search)(?:r\/\w+\/)?(\w{7}|\w{5})[bghlmrst]?)|(?:(?:https?:\\?\/\\?\/)?(?:www\.|[im]\.)?(?<!(?:i\.stack\.))imgur(?:\.|(?:.?dot.?)|.)?(?:com|io)(?:\\\/|\\|\/|.?slash|(?:.?slash|.?\/)(?:\w|[^\S\r\n])|%2F))(?:(?!gallery|search)(?:r\/\w+\/)?(?:(?:original|mp4)(?:\\\/|\\|\/|.?slash|(?:.?slash|.?\/)(?:\w|[^\S\r\n])|%2F).+?|download(?:\\\/|\\|\/|.?slash|(?:.?slash|.?\/)(?:\w|[^\S\r\n])|%2F)?)?(\w{7}|\w{5})[bghlmrst]?|(?:a\/?\\?(\w{7}|\w{5}))|(?:(?:gallery|t\/\w+)\/(\w{7}|\w{5}))|(?:(?:user\/([^\/?#]+)(?:\", )+?(?:\/posts|\/submitted)?\/?)|(?:user\/([^\/?#]+)(?:\/posts|\/submitted)\/?)|(?:user\/([^\/?#]+)(?:\/favorites)\/?))))", dump):
+    if i.group(1):
+      print('https://imgur.com/' + i.group(1))
+      f.write('https://imgur.com/' + i.group(1) + '\n')
+    if i.group(2):
+      print('https://i.stack.imgur.com/' + i.group(2) + '.png')
+      f.write('https://i.stack.imgur.com/' + i.group(2) + '.png' + '\n')
+    if i.group(3):
+      print('https://imgur.com/' + i.group(3))
+      f.write('https://imgur.com/' + i.group(3) + '\n')
+    if i.group(4):
+      print('https://imgur.com/a/' + i.group(4))
+      f.write('https://imgur.com/a/' + i.group(4) + '\n')
+    if i.group(5):
+      print('https://imgur.com/gallery/' + i.group(5))
+      f.write('https://imgur.com/gallery/' + i.group(5) + '\n')
+    if i.group(6):
+      print('https://imgur.com/user/' + i.group(6))
+      f.write('https://imgur.com/user/' + i.group(6) + '\n')
+    if i.group(7):
+      print('https://imgur.com/user/' + i.group(7) + '/posts')
+      f.write('https://imgur.com/user/' + i.group(7) + '/posts' + '\n')
+    if i.group(8):
+      print('https://imgur.com/user/' + i.group(8) + '/favorites')
+      f.write('https://imgur.com/user/' + i.group(8) + '/favorites' + '\n')
 print("Complete")
